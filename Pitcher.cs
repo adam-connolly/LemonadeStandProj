94e5458f2f0a9fa9c84c935fafb8dda2872d8ef9
Adam Connolly <adam.connolly66@gmail.com>
2020-03-20 11:42:39 -0500

    New Classes: Day, Weather, Game, Pitcher, Recipe, Customer

---
+++ Pitcher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand_3DayStarter
+{
+    class Pitcher : Item
+    {
+        //member variables
+
+        //constructor
+        public Pitcher()
+        {
+            name = "Pitcher";
+        }
+        //member methods
+    }
+}
